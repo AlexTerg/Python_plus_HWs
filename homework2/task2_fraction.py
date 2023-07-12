c1700c098f76478df921738b0f22732a61e589de
AlexTerg <s.tergoev@gmail.com>
2023-07-12 03:25:49 +0300

    .\homework2\task2_fraction.py

--- homework2/task2_fraction.py
+++ homework2/task2_fraction.py
@@ -12,7 +12,7 @@ def get_lcm(num1: int, num2: int) -> int:
     return num1 * num2 // math.gcd(num1, num2)
 
 
-def get_sum(fraction1: tuple, fraction2: tuple) -> tuple or int:
+def get_sum(fraction1: tuple, fraction2: tuple) -> str:
 
     num1, denom1 = fraction1
     num2, denom2 = fraction2
@@ -22,27 +22,26 @@ def get_sum(fraction1: tuple, fraction2: tuple) -> tuple or int:
 
     new_num1: int = int(lcm / denom1 * num1)
     new_num2: int = int(lcm / denom2 * num2)
-    
+
     # print(fractions.Fraction((new_num1 + new_num2) // lcm_num, lcm // lcm_num))
 
     if ((new_num1 + new_num2) // lcm_num) % (lcm // lcm_num) != 0:
         return f'{(new_num1 + new_num2) // lcm_num}/{lcm // lcm_num}'
     else:
         return str(((new_num1 + new_num2) // lcm_num) // (lcm // lcm_num))
-    
 
 
-def get_prod(fraction1: tuple, fraction2: tuple) -> tuple or int:
+def get_prod(fraction1: tuple, fraction2: tuple) -> str:
 
     num1, denom1 = fraction1
     num2, denom2 = fraction2
 
-    lcm = get_lcm(denom1, denom2)
-    lcm_num = get_lcm(num1, num2)
+    lcm: int = get_lcm(denom1, denom2)
+    lcm_num: int = get_lcm(num1, num2)
 
     new_num1: int = int(lcm / denom1 * num1)
     new_num2: int = int(lcm / denom2 * num2)
-    
+
     # print(fractions.Fraction((new_num1 * new_num2) // lcm_num, lcm // lcm_num))
 
     if ((new_num1 * new_num2) // lcm_num) % (lcm // lcm_num) != 0:
@@ -53,11 +52,7 @@ def get_prod(fraction1: tuple, fraction2: tuple) -> tuple or int:
 
 first_fraction: tuple = parse_fraction_number()
 second_fraction: tuple = parse_fraction_number()
-sum_fr = get_sum(first_fraction, second_fraction)
-prod_fr = get_prod(first_fraction, second_fraction)
+sum_fr: str = get_sum(first_fraction, second_fraction)
+prod_fr: str = get_prod(first_fraction, second_fraction)
 
 print(f'sum of fraction = {sum_fr} and product of fraction = {prod_fr}')
-
-
-
-# if first_value_denomerator > second_value_denomerator:
